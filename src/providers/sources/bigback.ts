e9061fc3708e1706dd4f9e856309a20966159926
Chargonium <svenvandoorn@icloud.com>
2025-06-11 23:20:51 +0200

    This better fucking work

--- src/providers/sources/bigback.ts
+++ src/providers/sources/bigback.ts
@@ -3,52 +3,51 @@ import { SourcererOutput, makeSourcerer } from '@/providers/base';
 import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
 import { NotFoundError } from '@/utils/errors';
 
-const baseUrl = 'https://bigback.dev.sudo-flix.nl';
-
 async function comboScraper(ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> {
-  const mediaType = ctx.media.type;
-  const movieId = ctx.media.tmdbId;
+  const baseUrl = 'https://bigback.dev.sudo-flix.nl';
+  let url: string;
 
-  const watchPageUrl =
-    mediaType === 'movie'
-      ? `${baseUrl}/movie/${movieId}`
-      : `${baseUrl}/show/${ctx.media.episode.tmdbId}/${ctx.media.season.number}/${ctx.media.episode.number}`;
-
-  ctx.progress(50);
-
-  const resp = await fetch(watchPageUrl, {
-  headers: {
-    'Range': 'bytes=0-511'
+  if (ctx.media.type === 'movie') {
+    url = `${baseUrl}/movie/${ctx.media.tmdbId}`;
+  } else {
+    url = `${baseUrl}/show/${ctx.media.tmdbId}/${ctx.media.season.number}/${ctx.media.episode.number}`;
   }
-});
 
-  if (!(resp.status >= 200 && resp.status < 300)) throw new NotFoundError('No media found.')
+  const data = await ctx.proxiedFetcher.full(url, {
+    headers: {
+      redirect: 'manual',
+      Range: 'bytes=0-511',
+    },
+  });
 
-  ctx.progress(90);
+  if (!(data.statusCode >= 200 && data.statusCode < 300)) throw new NotFoundError('No media found.');
+
+  const streamUrl = data.headers.get('location') || data.finalUrl;
+  if (!streamUrl) throw new NotFoundError('No media URL found.');
 
   return {
-      embeds: [],
-      stream: [
-        {
-          id: 'primary',
-          type: 'file',
-          qualities: {
-            720: {
-              type: 'mp4',
-              url: watchPageUrl
-            },
+    stream: [
+      {
+        id: 'primary',
+        captions: [],
+        qualities: {
+          unknown: {
+            type: 'mp4',
+            url: streamUrl,
           },
-          flags: [flags.CORS_ALLOWED],
-          captions: [],
         },
-      ],
+        type: 'file',
+        flags: [flags.CORS_ALLOWED],
+      },
+    ],
+    embeds: [],
   };
 }
 
 export const bigbackScraper = makeSourcerer({
   id: 'bigback',
-  name: 'bigback',
-  rank: 100000,
+  name: 'Bigback',
+  rank: 300,
   disabled: false,
   flags: [flags.CORS_ALLOWED],
   scrapeMovie: comboScraper,
